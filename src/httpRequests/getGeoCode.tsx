c71a25417083ab329ece6f1a2e3478ed73af9539
Ioannis Petsas <ioannispetsas@bbv.ch>
2024-01-26 00:02:43 +0200

    Https Fix

--- src/httpRequests/getGeoCode.tsx
+++ src/httpRequests/getGeoCode.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const getGeoCode = (city: string) => {
   return axios.get(
-    `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=12e3f69744a88112869dee430f7af36a`
+    `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=12e3f69744a88112869dee430f7af36a`
   );
 };
 
